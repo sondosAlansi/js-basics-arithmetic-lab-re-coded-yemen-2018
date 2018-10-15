cf2418e9f23f19b82122a41ac1c606ea13d78f4d
sondos alansi <sondos.alansi@yahoo.com>
2018-10-15 19:17:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ let newID=oldID + 1000000000;
 let ageIsValid=Number.isInteger(currentAge);
 let randomNumber=Math.random() * 20 + 1;
 let randomInteger=Math.floor(randomNumber);
-let randomUserID=1000000000+Math.floor(randomNumber);
+let randomUserID=1000000000+Math.randomInteger;
